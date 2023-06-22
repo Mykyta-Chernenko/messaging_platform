56e4cafbc211b168d197d9d55c8e3d0a6ef28fb7
Nikita Chernenko <nikita.chernenko@arundo.com>
2023-06-22 14:32:56 +0100

    chore: remove code for PR

--- auth.py
+++
@@ -1,8 +0,0 @@
-from db import DB
-from models import RequestMessage
-
-
-# TODO user_id comes from the Bearer token in the real scenario
-def get_user_from_request(db: DB, message: RequestMessage):
-    user_id = 2 if message.to_user_id == 1 else 1
-    return db.get_user(user_id)
